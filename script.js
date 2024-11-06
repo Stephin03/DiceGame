1543197d251a247b305a89172654dd9450e13945
Stephin S <138616969+Stephin03@users.noreply.github.com>
2024-11-06 14:14:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function rollDice(){
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    document.querySelector(".img1").src = "./dice" + randomNumber1 + ".png";
+    document.querySelector(".img1").src = "dice" + randomNumber1 + ".png";
 
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    document.querySelector(".img2").src = "./dice" + randomNumber2 + ".png";
+    document.querySelector(".img2").src = "dice" + randomNumber2 + ".png";
 
 
     if(randomNumber1>randomNumber2){
